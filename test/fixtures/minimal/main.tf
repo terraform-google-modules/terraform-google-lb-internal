746e0223b989fcd22df7096ca2b0c7ec38957fa3
omazin <osmansimf@rambler.ru>
2019-11-22 11:04:52 +0300

    Test. Set ip_address's default to null.

--- test/fixtures/minimal/main.tf
+++ test/fixtures/minimal/main.tf
@@ -49,12 +49,6 @@ locals {
   }
 }
 
-resource "google_project_service" "test" {
-  project            = var.project_id
-  service            = "compute.googleapis.com"
-  disable_on_destroy = "false"
-}
-
 resource "google_compute_network" "test" {
   project                 = var.project_id
   name                    = local.resource_name
