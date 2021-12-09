d0b5ec825fd1cca424088600d96435367af4e032
upodroid <cy@borg.dev>
2021-12-09 13:10:02 +0000

    upgrade to v4

---
+++ examples/minimal/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 2.0"
+    }
+  }
+  required_version = ">= 0.13"
+}
