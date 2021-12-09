a57ecf3f932d5773a1006578dfb7efc11c4ce422
upodroid <cy@borg.dev>
2021-12-09 19:36:41 +0000

    revert some changes

--- test/fixtures/minimal/main.tf
+++ test/fixtures/minimal/main.tf
@@ -15,57 +15,7 @@
  */
 
 module "example" {
-  source       = "../../../"
-  project      = var.project_id
-  network      = google_compute_network.test.name
-  subnetwork   = google_compute_subnetwork.test.name
-  region       = var.region
-  name         = local.resource_name
-  ports        = ["8080"]
-  source_tags  = ["source-tag-foo"]
-  target_tags  = ["target-tag-bar"]
-  backends     = []
-  health_check = local.health_check
-}
-
-resource "random_string" "suffix" {
-  length  = 8
-  upper   = false
-  lower   = true
-  number  = true
-  special = false
-}
-
-locals {
-  random_suffix = random_string.suffix.result
-  resource_name = "ilb-minimal-${local.random_suffix}"
-  health_check = {
-    type                = "http"
-    check_interval_sec  = 1
-    healthy_threshold   = 4
-    timeout_sec         = 1
-    unhealthy_threshold = 5
-    response            = ""
-    proxy_header        = "NONE"
-    port                = 8081
-    port_name           = "health-check-port"
-    request             = ""
-    request_path        = "/"
-    host                = "1.2.3.4"
-    enable_log          = false
-  }
-}
-
-resource "google_compute_network" "test" {
-  project                 = var.project_id
-  name                    = local.resource_name
-  auto_create_subnetworks = false
-}
-
-resource "google_compute_subnetwork" "test" {
-  project       = var.project_id
-  name          = local.resource_name
-  network       = google_compute_network.test.name
-  region        = var.region
-  ip_cidr_range = "10.2.0.0/16"
+  source     = "../../../examples/minimal"
+  region     = var.region
+  project_id = var.project_id
 }
